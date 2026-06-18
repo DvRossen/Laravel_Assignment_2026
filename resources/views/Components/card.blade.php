e42bc52edf353d34d7ce328ecf1c0c99025409ec
Danz <DvRossen@outlook.com>
2026-06-18 17:44:54 +0200

    finished template for card

--- resources/views/Components/card.blade.php
+++ resources/views/Components/card.blade.php
@@ -2,40 +2,59 @@
 <div class="bg-red-500 w-[500px] h-[260px]">
 
 <!-- Top Wrapper -->
-<div class=" flex bg-blue-500 rounded-xl w-full h-[200px] mb-[10px] overflow-hidden">
+    <div class=" flex bg-blue-500 rounded-xl w-full h-[200px] mb-[10px] overflow-hidden">
 
 <!-- Left Side Wrapper -->
-<div class="w-[250px] h-full">
-<img src="" class="bg-orange-500 w-full h-full "></img>
-</div>
+    <div class="w-[250px] h-full">
+        <img src="" class="bg-orange-500 w-full h-full "></img> <!-- chosen background here --> 
+    </div>
 
 <!-- Right Side Wrapper -->
-<div class="flex flex-col w-[250px] px-[1rem] py-[0.5rem]">
+    <div class="flex flex-col w-[250px] px-[1rem] py-[0.5rem]">
 
 <!-- Title & Icon Wrapper -->
-<div class="flex border-b-3 justify-between">
-<h2 class="text-3xl">Test</h2>
-<img class="bg-purple-500 size-[30px]"></img>
-</div>
+        <div class="flex border-b-3 justify-between h-[40px] items-center">
+            <h2 class="text-3xl">Test</h2> <!-- title here -->
+            <img class="bg-purple-500 size-[30px] aspect 1/1"></img> <!-- chosen icon here --> 
+        </div>
 
 <!-- Description -->
-<div class="border-b-3">
-<p class="text-[1rem] overflow-hidden text-elipses">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt quia libero maiores. Quia doloremque est voluptas quis itaque assumenda officiis obcaecati hic beatae! Earum nulla iusto in, nesciunt id nisi!</p>
-</div>
+        <div class="border-b-3">
+            <p class="line-clamp-5 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate mollitia optio perferendis commodi at id inventore veniam, facere officiis corrupti quidem, expedita ad magnam voluptatem ea impedit perspiciatis. Aliquam, laudantium!
+            </p>
+        </div>
 
 <!-- Time & Date Wrapper -->
-<div class="flex justify-center">
-<h3 class="text-2xl">Date</h3>
-<h3 class="text-2xl">Time</h3>
-</div>
+        <div class="flex justify-center">
+            <h3 class="text-2xl">Date</h3> <!-- chosen date here --> 
+            <p class="text-2xl font-bold">&nbsp;•&nbsp;</p>
+            <h3 class="text-2xl">Time</h3> <!-- chosen time here --> 
+        </div>
 
-</div>
-</div>
+    </div>
+    </div>
 
 
 
 <!--Bottom Wrapper -->
-<div class="bg-green-500 rounded-xl w-full h-[50px]">
-</div>
+    <div class="flex bg-green-500 rounded-xl w-full h-[50px]">
+        <div class="px-[1rem] h-full w-[250px] flex items-center">
+            <img class="bg-purple-500 size-[30px] aspect-1/1"></img> <!-- crown/star/pencil (idk yet) here --> 
+            <h3 class="line-clamp-2"> Made By</h3> <!-- card owner here -->
+        </div>
+        <div class="pr-[1rem] h-full w-[250px] flex justify-between">
+            <div class="flex items-center">
+                <p class="text-2xl font-bold">#</p> <!-- number of atendees here -->
+                <img class="bg-purple-500 size-[30px] aspect-1/1"></img> <!-- person svg here -->
+                <h3>test</h3> <!-- Names of atendees here-->
+            </div>
+            <div class="flex items-center">
+                <a href="/"  >
+                <img class=" bg-purple-500 size-[30px] aspect-1/1 hover-click"></img>
+                </a>
+            </div>
+        </div>
+    </div>
 
 </div>
